b39e987e389fef75d8fefb2e8fae5fe473df3fa2
NoireRos <121197315+NoireRos@users.noreply.github.com>
2023-02-11 19:08:15 +0800

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearallButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.remainderbutton = new System.Windows.Forms.Button();
+            this.oneButton = new System.Windows.Forms.Button();
+            this.twoButton = new System.Windows.Forms.Button();
+            this.threeButton = new System.Windows.Forms.Button();
+            this.timesButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.sixButton = new System.Windows.Forms.Button();
+            this.fiveButton = new System.Windows.Forms.Button();
+            this.fourButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.nineButton = new System.Windows.Forms.Button();
+            this.eightButton = new System.Windows.Forms.Button();
+            this.sevenButton = new System.Windows.Forms.Button();
+            this.equalsButton = new System.Windows.Forms.Button();
+            this.pointButton = new System.Windows.Forms.Button();
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.UserInputText = new System.Windows.Forms.TextBox();
+            this.notifLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clearallButton
+            // 
+            this.clearallButton.Location = new System.Drawing.Point(3, 70);
+            this.clearallButton.Name = "clearallButton";
+            this.clearallButton.Size = new System.Drawing.Size(91, 72);
+            this.clearallButton.TabIndex = 0;
+            this.clearallButton.Text = "CE";
+            this.clearallButton.UseVisualStyleBackColor = true;
+            this.clearallButton.Click += new System.EventHandler(this.clearallButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(190, 70);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(91, 72);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Del";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // remainderbutton
+            // 
+            this.remainderbutton.Location = new System.Drawing.Point(287, 70);
+            this.remainderbutton.Name = "remainderbutton";
+            this.remainderbutton.Size = new System.Drawing.Size(91, 72);
+            this.remainderbutton.TabIndex = 2;
+            this.remainderbutton.Text = "%";
+            this.remainderbutton.UseVisualStyleBackColor = true;
+            this.remainderbutton.Click += new System.EventHandler(this.remainderbutton_Click);
+            // 
+            // oneButton
+            // 
+            this.oneButton.Location = new System.Drawing.Point(3, 148);
+            this.oneButton.Name = "oneButton";
+            this.oneButton.Size = new System.Drawing.Size(91, 72);
+            this.oneButton.TabIndex = 3;
+            this.oneButton.Text = "1";
+            this.oneButton.UseVisualStyleBackColor = true;
+            this.oneButton.Click += new System.EventHandler(this.oneButton_Click);
+            // 
+            // twoButton
+            // 
+            this.twoButton.Location = new System.Drawing.Point(97, 148);
+            this.twoButton.Name = "twoButton";
+            this.twoButton.Size = new System.Drawing.Size(91, 72);
+            this.twoButton.TabIndex = 4;
+            this.twoButton.Text = "2";
+            this.twoButton.UseVisualStyleBackColor = true;
+            this.twoButton.Click += new System.EventHandler(this.twoButton_Click);
+            // 
+            // threeButton
+            // 
+            this.threeButton.Location = new System.Drawing.Point(190, 148);
+            this.threeButton.Name = "threeButton";
+            this.threeButton.Size = new System.Drawing.Size(91, 72);
+            this.threeButton.TabIndex = 5;
+            this.threeButton.Text = "3";
+            this.threeButton.UseVisualStyleBackColor = true;
+            this.threeButton.Click += new System.EventHandler(this.threeButton_Click);
+            // 
+            // timesButton
+            // 
+            this.timesButton.Location = new System.Drawing.Point(287, 148);
+            this.timesButton.Name = "timesButton";
+            this.timesButton.Size = new System.Drawing.Size(91, 72);
+            this.timesButton.TabIndex = 6;
+            this.timesButton.Text = "X";
+            this.timesButton.UseVisualStyleBackColor = true;
+            this.timesButton.Click += new System.EventHandler(this.timesButton_Click);
+            // 
+            // minusButton
+            // 
+            this.minusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusButton.Location = new System.Drawing.Point(287, 226);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(91, 72);
+            this.minusButton.TabIndex = 10;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // sixButton
+            // 
+            this.sixButton.Location = new System.Drawing.Point(190, 226);
+            this.sixButton.Name = "sixButton";
+            this.sixButton.Size = new System.Drawing.Size(91, 72);
+            this.sixButton.TabIndex = 9;
+            this.sixButton.Text = "6";
+            this.sixButton.UseVisualStyleBackColor = true;
+            this.sixButton.Click += new System.EventHandler(this.sixButton_Click);
+            // 
+            // fiveButton
+            // 
+            this.fiveButton.Location = new System.Drawing.Point(97, 226);
+            this.fiveButton.Name = "fiveButton";
+            this.fiveButton.Size = new System.Drawing.Size(91, 72);
+            this.fiveButton.TabIndex = 8;
+            this.fiveButton.Text = "5";
+            this.fiveButton.UseVisualStyleBackColor = true;
+            this.fiveButton.Click += new System.EventHandler(this.fiveButton_Click);
+            // 
+            // fourButton
+            // 
+            this.fourButton.Location = new System.Drawing.Point(3, 226);
+            this.fourButton.Name = "fourButton";
+            this.fourButton.Size = new System.Drawing.Size(91, 72);
+            this.fourButton.TabIndex = 7;
+            this.fourButton.Text = "4";
+            this.fourButton.UseVisualStyleBackColor = true;
+            this.fourButton.Click += new System.EventHandler(this.fourButton_Click);
+            // 
+            // plusButton
+            // 
+            this.plusButton.Location = new System.Drawing.Point(287, 304);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(91, 72);
+            this.plusButton.TabIndex = 14;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // nineButton
+            // 
+            this.nineButton.Location = new System.Drawing.Point(190, 304);
+            this.nineButton.Name = "nineButton";
+            this.nineButton.Size = new System.Drawing.Size(91, 72);
+            this.nineButton.TabIndex = 13;
+            this.nineButton.Text = "9";
+            this.nineButton.UseVisualStyleBackColor = true;
+            this.nineButton.Click += new System.EventHandler(this.nineButton_Click);
+            // 
+            // eightButton
+            // 
+            this.eightButton.Location = new System.Drawing.Point(97, 304);
+            this.eightButton.Name = "eightButton";
+            this.eightButton.Size = new System.Drawing.Size(91, 72);
+            this.eightButton.TabIndex = 12;
+            this.eightButton.Text = "8";
+            this.eightButton.UseVisualStyleBackColor = true;
+            this.eightButton.Click += new System.EventHandler(this.eightButton_Click);
+            // 
+            // sevenButton
+            // 
+            this.sevenButton.Location = new System.Drawing.Point(3, 304);
+            this.sevenButton.Name = "sevenButton";
+            this.sevenButton.Size = new System.Drawing.Size(91, 72);
+            this.sevenButton.TabIndex = 11;
+            this.sevenButton.Text = "7";
+            this.sevenButton.UseVisualStyleBackColor = true;
+            this.sevenButton.Click += new System.EventHandler(this.sevenButton_Click);
+            // 
+            // equalsButton
+            // 
+            this.equalsButton.Location = new System.Drawing.Point(287, 382);
+            this.equalsButton.Name = "equalsButton";
+            this.equalsButton.Size = new System.Drawing.Size(91, 72);
+            this.equalsButton.TabIndex = 17;
+            this.equalsButton.Text = "=";
+            this.equalsButton.UseVisualStyleBackColor = true;
+            this.equalsButton.Click += new System.EventHandler(this.equalsButton_Click);
+            // 
+            // pointButton
+            // 
+            this.pointButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.pointButton.Location = new System.Drawing.Point(190, 382);
+            this.pointButton.Name = "pointButton";
+            this.pointButton.Size = new System.Drawing.Size(91, 72);
+            this.pointButton.TabIndex = 16;
+            this.pointButton.Text = ".";
+            this.pointButton.UseVisualStyleBackColor = true;
+            this.pointButton.Click += new System.EventHandler(this.pointButton_Click);
+            // 
+            // zeroButton
+            // 
+            this.zeroButton.Location = new System.Drawing.Point(97, 382);
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.Size = new System.Drawing.Size(91, 72);
+            this.zeroButton.TabIndex = 15;
+            this.zeroButton.Text = "0";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            this.zeroButton.Click += new System.EventHandler(this.zeroButton_Click);
+            // 
+            // UserInputText
+            // 
+            this.UserInputText.Location = new System.Drawing.Point(13, 13);
+            this.UserInputText.Name = "UserInputText";
+            this.UserInputText.Size = new System.Drawing.Size(365, 22);
+            this.UserInputText.TabIndex = 18;
+            // 
+            // notifLabel
+            // 
+            this.notifLabel.AutoSize = true;
+            this.notifLabel.Location = new System.Drawing.Point(13, 42);
+            this.notifLabel.Name = "notifLabel";
+            this.notifLabel.Size = new System.Drawing.Size(266, 16);
+            this.notifLabel.TabIndex = 19;
+            this.notifLabel.Text = "Please enter an equation or press enter or =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 476);
+            this.Controls.Add(this.notifLabel);
+            this.Controls.Add(this.UserInputText);
+            this.Controls.Add(this.equalsButton);
+            this.Controls.Add(this.pointButton);
+            this.Controls.Add(this.zeroButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.nineButton);
+            this.Controls.Add(this.eightButton);
+            this.Controls.Add(this.sevenButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.sixButton);
+            this.Controls.Add(this.fiveButton);
+            this.Controls.Add(this.fourButton);
+            this.Controls.Add(this.timesButton);
+            this.Controls.Add(this.threeButton);
+            this.Controls.Add(this.twoButton);
+            this.Controls.Add(this.oneButton);
+            this.Controls.Add(this.remainderbutton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.clearallButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearallButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button remainderbutton;
+        private System.Windows.Forms.Button oneButton;
+        private System.Windows.Forms.Button twoButton;
+        private System.Windows.Forms.Button threeButton;
+        private System.Windows.Forms.Button timesButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button sixButton;
+        private System.Windows.Forms.Button fiveButton;
+        private System.Windows.Forms.Button fourButton;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button nineButton;
+        private System.Windows.Forms.Button eightButton;
+        private System.Windows.Forms.Button sevenButton;
+        private System.Windows.Forms.Button equalsButton;
+        private System.Windows.Forms.Button pointButton;
+        private System.Windows.Forms.Button zeroButton;
+        private System.Windows.Forms.TextBox UserInputText;
+        private System.Windows.Forms.Label notifLabel;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    /// <summary>
+    /// A Basic Calculator
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Clears the User Input Text
+        /// </summary>
+        /// <param name="sender">event sender</param>
+        /// <param name="e">The event argument</param>
+        private void clearallButton_Click(object sender, EventArgs e)
+        {
+            ///Clears the text from the user input text box
+            this.UserInputText.Text = String.Empty;
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // Delete the value of the selection position
+            DeleteTextValue();
+            
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Focuses on the input text
+        /// </summary>
+        private void FocusInputText()
+        { 
+            this.UserInputText.Focus();
+        }
+
+        private void InsertTextValue(string value)
+        {
+            //Remember selection start
+            var selectionStart = this.UserInputText.SelectionStart;
+
+            // set selection text
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+
+            //Restore Selection Text
+            this.UserInputText.SelectionStart = selectionStart + value.Length;
+
+            //Set Selection length to 0
+            this.UserInputText.SelectionLength = 0;
+        }
+
+
+        /// <summary>
+        /// Delete the character to the right of the selection start of the user input text box
+        /// </summary>
+        private void DeleteTextValue() 
+        {
+            // if we don't have a value to delete, return
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+
+            //Remember Selection start
+            var selectionStart = this.UserInputText.SelectionStart;
+
+            // delete the right character to the right of the selection
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+
+            //restore the selection start
+            this.UserInputText.SelectionStart = selectionStart;
+
+            // set selection length to 0
+            this.UserInputText.SelectionLength = 0;
+
+        }
+
+
+        /// <summary>
+        /// Adds the zero character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("0");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the dot character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void pointButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue(".");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the total value of character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void equalsButton_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// calculate the given equation and outputs to the user label
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void CalculateEquation()
+        {
+            //TODO: Finish
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the plus character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("+");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the nine character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void nineButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("9");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the eight character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void eightButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("8");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the seven character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("7");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the six character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void sixButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("6");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the five character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void fiveButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("5");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the four character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void fourButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("4");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the times character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void timesButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("*");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the three character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void threeButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("3");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the two character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void twoButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("2");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the one character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void oneButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("1");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the remainder character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void remainderbutton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("%");
+
+            //Focus the user input text
+            FocusInputText();
+        }
+
+
+        /// <summary>
+        /// Adds the minus character to the text at the currently selection position
+        /// </summary>
+        /// <param name="sender"> The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("-");
+
+            //Focus the user input text
+            FocusInputText();   
+        }
+    }
+}
